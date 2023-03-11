e894923226fef65232628043bfde5f2e5d10299b
BOUVERON Armand <armand.bouveron@telecomnancy.eu>
2023-03-11 23:14:54 +0100

    feat: login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,115 @@
+<nav
+	class="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600"
+>
+	<div class="container flex flex-wrap items-center justify-between mx-auto">
+		<a href="/" class="flex items-center">
+			<img src="favicon.png" class="h-6 mr-3 sm:h-9" alt="Favicon.png" />
+			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+				>Multdle</span
+			>
+		</a>
+		<div class="flex md:order-2">
+			<button
+				type="button"
+				class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+				>Play</button
+			>
+			<button
+				data-collapse-toggle="navbar-sticky"
+				type="button"
+				class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+				aria-controls="navbar-sticky"
+				aria-expanded="false"
+			>
+				<span class="sr-only">Open main menu</span>
+				<svg
+					class="w-6 h-6"
+					aria-hidden="true"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+					><path
+						fill-rule="evenodd"
+						d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+						clip-rule="evenodd"
+					/></svg
+				>
+			</button>
+		</div>
+		<div
+			class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+			id="navbar-sticky"
+		>
+			<ul
+				class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+			>
+				<li>
+					<a
+						href="/"
+						class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+						aria-current="page">Home</a
+					>
+				</li>
+				<li>
+					<a
+						href="/rooms"
+						class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+						>Rooms</a
+					>
+				</li>
+				<li>
+					<a
+						href="/login"
+						class="block py-2 pl-3 pr-4 text-white class:bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+						>Login</a
+					>
+				</li>
+				<li>
+					<a
+						href="/signup"
+						class="block py-2 pl-3 pr-4 text-gray-700 class:bg-blue-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+						>Sign Up</a
+					>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<form style="padding-left: 20px; padding-right: 20px; padding-top: 100px" method="POST">
+	<div class="relative z-0 w-1/4 mb-6 group">
+		<input
+			type="email"
+			name="floating_email"
+			id="floating_email"
+			class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+			placeholder=" "
+			required
+		/>
+		<label
+			for="floating_email"
+			class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+			>Username</label
+		>
+	</div>
+	<div class="relative z-0 w-1/4 mb-6 group">
+		<input
+			type="password"
+			name="floating_password"
+			id="floating_password"
+			class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+			placeholder=" "
+			required
+		/>
+		<label
+			for="floating_password"
+			class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+			>Password</label
+		>
+	</div>
+	<button
+		type="submit"
+		class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+		>Login</button
+	>
+</form>
