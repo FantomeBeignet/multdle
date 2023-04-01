16a7c0b915b41bfc6d000cacdc26f15776edbe5b
Tom Béné <himself@fantomebeig.net>
2023-04-02 00:00:29 +0200

    refactor: limited calls to trpc + redis

--- src/lib/pusher/client.ts
+++ src/lib/pusher/client.ts
@@ -3,7 +3,15 @@ import { default as PusherClient } from 'pusher-js';
 const PUSHER_APP_KEY = '6993c4f08815bfb293df';
 const PUSHER_CLUSTER = 'eu';
 
-export const pusherClient = new PusherClient(PUSHER_APP_KEY, {
-	cluster: PUSHER_CLUSTER,
-	authEndpoint: '/api/pusher/auth'
-});
+export const pusherClient = (username: string) =>
+	new PusherClient(PUSHER_APP_KEY, {
+		cluster: PUSHER_CLUSTER,
+		authEndpoint: '/api/pusher/auth',
+		userAuthentication: {
+			params: {
+				username: username
+			},
+			endpoint: '/api/pusher/signin',
+			transport: 'ajax'
+		}
+	});
