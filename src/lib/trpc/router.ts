aa907a3c89d3b0b64b7e5e6b698761721c0acaea
Tom Béné <himself@fantomebeig.net>
2023-04-02 00:17:59 +0200

    feat: function to clean unused data in redis

--- src/lib/trpc/router.ts
+++ src/lib/trpc/router.ts
@@ -1,9 +1,11 @@
 import { t } from '$lib/trpc/t';
 import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
 import { rooms } from '$lib/trpc/routes/rooms';
+import { sockets } from '$lib/trpc/routes/sockets';
 
 export const router = t.router({
-	rooms
+	rooms,
+	sockets
 });
 
 export const caller = router.createCaller({});
