1b6e33ae8dd76841e99fab4fe31adfe66d7a454b
Tom Béné <himself@fantomebeig.net>
2023-04-02 15:12:20 +0200

    feat: endpoint to get word

--- src/lib/trpc/routes/games.ts
+++ src/lib/trpc/routes/games.ts
@@ -4,7 +4,7 @@ import { redis } from '$lib/redis';
 import { caller } from '$lib/trpc/router';
 import { pusherServer } from '$lib/pusher/server';
 import { getRandomWord } from '$lib/words';
-import { createHash } from 'crypto';
+import { createHash, getRandomValues } from 'crypto';
 
 export const games = t.router({
 	start: t.procedure.input(z.string()).mutation(async ({ input }) => {
@@ -21,6 +21,11 @@ export const games = t.router({
 		const minutes = date.getMinutes();
 		const gameSeed = parseInt(createHash('sha256').update(input).digest('hex'), 16);
 		const seed = gameSeed + hours + minutes;
+		return getRandomWord(seed);
+	}),
+	getSoloWord: t.procedure.query(() => {
+		const seed = getRandomValues(new Int16Array(1))[0];
+		console.log(seed);
 		return getRandomWord(seed);
 	})
 });
