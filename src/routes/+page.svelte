0f7fafff34163752a0a99632ec08533e938503d6
Tom Béné <himself@fantomebeig.net>
2023-04-02 14:11:37 +0200

    refactor: removed unused import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,4 @@
 <script>
-	import { trpc } from '$lib/trpc/client';
-	import { page } from '$app/stores';
-
 	import { setContext } from 'svelte';
 	const blue_signup = true;
 	setContext('SignupContext', { blue_signup });
