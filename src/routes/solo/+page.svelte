fdf3dc5196c8fc41d61b40c2457a844bb890cecd
Tom Béné <himself@fantomebeig.net>
2023-04-02 15:15:57 +0200

    feat: base for solo page

---
+++ src/routes/solo/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let data;
+</script>
+
+<div class="flex items-center justify-center w-full">
+	<div class="flex flex-col items-center justify-center w-full p-10 max-w-4xl lg:px-16">
+		<h1
+			class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center"
+		>
+			Solo play
+		</h1>
+	</div>
+</div>
