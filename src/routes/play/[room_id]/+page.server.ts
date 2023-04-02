9a88ca80715812ba7ac3de70b9a1019335a6972c
Tom Béné <himself@fantomebeig.net>
2023-04-02 02:53:43 +0200

    feat: base for play page

---
+++ src/routes/play/[room_id]/+page.server.ts
@@ -0,0 +1,13 @@
+import { error } from '@sveltejs/kit';
+import { caller } from '$lib/trpc/router';
+
+/** @type {import('./$types').PageLoad} */
+export async function load({ params }) {
+	if (await caller.games.exists(params.room_id)) {
+		return {
+			roomName: params.room_id
+		};
+	}
+
+	throw error(404, 'Not found');
+}
