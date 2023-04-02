fef5e8ff9d9737d708260301a18d68333871da41
Tom Béné <himself@fantomebeig.net>
2023-04-02 11:45:26 +0200

    fix: correct room name

--- src/lib/trpc/routes/games.ts
+++ src/lib/trpc/routes/games.ts
@@ -6,8 +6,8 @@ import { pusherServer } from '$lib/pusher/server';
 
 export const games = t.router({
 	start: t.procedure.input(z.string()).mutation(async ({ input }) => {
+		pusherServer.trigger(`presence-room-${input}`, 'start', {});
 		caller.rooms.delete(input);
-		pusherServer.trigger(input, 'start', {});
 		return await redis.sadd('games', input);
 	}),
 	exists: t.procedure.input(z.string()).query(async ({ input }) => {
