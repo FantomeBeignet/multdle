4ab474b86f40af72a777ce68484d959f9d13e987
Tom Béné <himself@fantomebeig.net>
2023-04-02 12:54:39 +0200

    feat: endpoint to get word for room

--- src/lib/trpc/routes/games.ts
+++ src/lib/trpc/routes/games.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 import { redis } from '$lib/redis';
 import { caller } from '$lib/trpc/router';
 import { pusherServer } from '$lib/pusher/server';
+import { getRandomWord } from '$lib/words';
 
 export const games = t.router({
 	start: t.procedure.input(z.string()).mutation(async ({ input }) => {
@@ -12,5 +13,14 @@ export const games = t.router({
 	}),
 	exists: t.procedure.input(z.string()).query(async ({ input }) => {
 		return await redis.sismember('games', input);
+	}),
+	getWord: t.procedure.input(z.string()).query(async ({ input }) => {
+		let seed: number;
+		const redisSeed = await redis.get(`game:seed:${input}`);
+		if (!redisSeed) {
+			seed = crypto.getRandomValues(new Int16Array(1))[0];
+			await redis.set(`game:seed:${input}`, seed, 'EX', 10);
+		} else seed = parseInt(redisSeed);
+		return getRandomWord(seed);
 	})
 });
